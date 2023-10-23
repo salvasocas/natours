9450fb54b02eeaa7ea5ebbe23d4e3cfe1251bd9e
Salvatore Socas <salvasocas02@hotmail.com>
2023-10-23 15:58:53 -0400

    changed booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,7 +58,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -79,7 +79,7 @@ exports.webhookCheckout = (req, res, next) => {
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
-  res.status(200).json({ received: true });
+  res.status(200).json({ received: 'si lo recibi' });
 };
 
 exports.createBooking = factory.createOne(Booking);
